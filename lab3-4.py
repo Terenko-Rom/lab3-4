9610897e7b29cdaf81961bfb1c54b5b7acdc4658
Terenko-Rom <romanterenko0@gmail.com>
2024-09-21 15:34:01 +0300

    ready

---
+++ lab3-4.py
@@ -0,0 +1,7 @@
+num=input("чотирьохзначне число")
+a=float(num[1])
+b=float(num[2])
+sum=a+b
+prod=a*b
+print (sum)
+print (prod)
---
+++ lab3-42.py
@@ -0,0 +1,9 @@
+catet1=input("catet1")
+catet2=input("catet2")
+height=input("height")
+a=float (catet1)
+b=float (catet2)
+c=float (height)
+S=a*b/2
+V=S*c
+print(V)
